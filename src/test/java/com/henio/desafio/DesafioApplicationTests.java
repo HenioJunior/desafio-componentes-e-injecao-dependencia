902b004ed89151b4c3308894457300684ad46391
Henio Junior <henioajunior@gmail.com>
2024-11-18 16:23:13 -0300

    first commit

---
+++ src/test/java/com/henio/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henio.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
